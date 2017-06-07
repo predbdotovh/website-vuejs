0263fb5dc13ef90998a0d8098750a5361041844f
predbdotovh <i@predb.ovh>
2017-06-07 15:39:20 +0200

    Added nuke info in TableRow

--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -4,10 +4,11 @@
       <span class="block cat">
         <router-link :to="{name: 'search', query: {q: '@cat ' + r.cat}}">{{r.cat}}</router-link>
       </span>
-      <span class="block text-break" v-if="r.genre">{{r.genre}}</span>
+      <span class="block text-break genre" v-if="r.genre">{{r.genre.substring(0, 36)}}</span>
     </td>
     <td>
       <span class="text-break name">{{r.name.replace('-' + r.team, '')}}<router-link class="text-italic" :to="{name: 'search', query: {q: '@team ' + r.team}}">-{{r.team}}</router-link></span>
+      <span class="nuke" :class="{'nuketype-nuke': nukeNuke.includes(r.nuke.type), 'nuketype-fine': nukeFine.includes(r.nuke.type)}" v-if="r.nuke">{{r.nuke.reason}} - <span class="nuke-net">{{r.nuke.net}}</span></span>
     </td>
     <td class="hide-sm date">
       <span v-if="r.preAt">{{new Date(r.preAt * 1000).toLocaleString()}}</span>
@@ -23,6 +24,12 @@
 <script>
 export default {
   name: 'table-row',
+  data () {
+    return {
+      nukeFine: ['unnuke', 'undelpre'],
+      nukeNuke: ['nuke', 'modnuke', 'delpre']
+    }
+  },
   props: {
     r: {
       type: Object,
@@ -33,7 +40,15 @@ export default {
 </script>
 
 <style>
-
+.nuketype-nuke {
+  color: #e85600;
+}
+.nuketype-fine {
+  color: #32b643;
+}
+.nuke-net {
+  font-style: italic;
+}
 .files-symbol {
   color: #32b643;
 }
